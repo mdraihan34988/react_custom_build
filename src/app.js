78ff84b849ebe124e625a64adc5b252004be9716
mdraihan34988 <swammia908@gmail.com>
2022-04-11 02:55:40 +0600

    react manual build

---
+++ src/app.js
@@ -0,0 +1,12 @@
+import React from "react";
+import "App.css"
+import ImageFile from "./download.png"
+
+const App = () => (
+    <div>
+        <h1>Manual Set Up of React Project</h1>
+        <div><img className="center" src={ImageFile} alt="image"/></div>
+    </div>
+);
+
+export default App;
